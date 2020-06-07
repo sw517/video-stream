dd76259b1b4c9f277a4fb724d134a086f7e2360a
Sam Wood <sam.wood@itech.media>
2020-06-07 12:00:34 +0100

    Chore - adds initial file for VideoStream component with basic functionality

---
+++ src/components/VideoStream.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="video-stream">
+    <video ref="video" class="video-js" />
+  </div>
+</template>
+
+<script>
+import videojs from 'video.js';
+
+export default {
+  name: 'VideoStream',
+  props: {
+    options: {
+      type: Object,
+      required: true,
+      default: () => {},
+    },
+  },
+  data() {
+    return {
+      player: null,
+    };
+  },
+  mounted() {
+    this.player = videojs(
+      this.$refs.video,
+      this.options,
+      function onPlayerReady() {
+        console.log('ready', this);
+      }
+    );
+  },
+  beforeDestroy() {
+    if (this.player) this.player.dispose();
+  },
+};
+</script>
+
+<style lang="scss"></style>
