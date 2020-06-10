ce77de08b5636cbb185297e3f683ced64bb01d98
Sam Wood <sam.wood@itech.media>
2020-06-10 20:56:52 +0100

    Chore - updates tailwind screen size config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     content: ['./public/**/*.html', './src/**/*.vue'],
   },
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: '600px',
+        md: '980px',
+        lg: '1200px',
+        xl: '1600px',
+      },
+    },
   },
   variants: {},
   plugins: [],
